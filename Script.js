d63d9be23470dc88e4bb7c6f1c5ccac819049339
Fariah Yusuf <fariahyusuf@Fariahs-MacBook-Pro.local>
2025-04-21 16:27:19 -0400

    class and Constructor without comment

---
+++ Script.js
@@ -0,0 +1,9 @@
+// Base Class: ProductProperties
+class ProductProperties {
+    //Constructor Method
+    constructor(name, price, quantity) {
+        this.name = name; //Name of the product.
+        this.price = price; //Price of the product.
+        this.quantity = quantity; //Quantity available in stock.
+    }
+}
